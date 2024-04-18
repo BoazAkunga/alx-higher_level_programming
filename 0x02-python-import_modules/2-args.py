e3ebd59432368fd9dabd10678102dd6387bbb40e
BoazAkunga <boazakunga@gmail.com>
2024-04-18 11:14:23 -0700

    2-args.py soln

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-import sys
+if __name__== "__main__":
+    from sys import argv
+    b = len(argv) - 1
+    if b < 1:
+        print("{} arguments.".format(b))
+    elif b == 1:
+        print("{} argument:".format(b))
+    else:
+        print("{} arguments:".format(b))
 
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range(b):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
