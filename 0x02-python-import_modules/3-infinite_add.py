698d81eb7bc4cae54e8bc5421bf29628c941e9fa
BoazAkunga <boazakunga@gmail.com>
2024-04-18 12:31:55 -0700

    3-infinite_add.py soln

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == "__main__":
+    from sys import argv
+    sumin = 0
+    for b in range(1, len(argv)):
+        sumin += int(argv[b])
+    print("{}".format(sumin))
